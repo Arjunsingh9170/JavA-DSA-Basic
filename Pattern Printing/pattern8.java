9bd2820e405b4dcc341957c8f045d363b5915df1
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-04 15:14:59 +0530

    Add files via upload

---
+++ Pattern Printing/pattern8.java
@@ -0,0 +1,13 @@
+public class pattern8{
+  public static void main (String args[]){
+    char i;
+    char j;
+
+    for(i='A'; i<='B'; i++){
+      for(j='A'; j<='B'; j++){
+        System.out.print(i);
+      }
+      System.out.print("\n");
+    }
+  }
+}
