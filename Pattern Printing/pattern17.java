caae8fc7ae444cf421adb271eda7f76516157dca
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-08 12:16:29 +0530

    Add files via upload

---
+++ Pattern Printing/pattern17.java
@@ -0,0 +1,11 @@
+public class pattern17{
+  public static void main(String args[]){
+    int n=5;
+    for(int i=1; i<=n; i++){
+      for(int j=1; j<=n; j++){
+        System.out.printf("%2d ",(2*(i+j))-3);
+      }
+      System.out.println();
+    }
+  }
+}
