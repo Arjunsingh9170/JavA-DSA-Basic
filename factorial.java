7570b06b53d5a7b6b9b614cb23d153270e3d4b9b
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-09 22:51:11 +0530

    Add files via upload

--- factorial.java
+++ factorial.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
-public class factorial{
+import java.util.*;
+public class factorial {
+  public static void factorial(int n){
 
-  public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a number:");
-    int n = sc.nextInt();
-    long fact=1;
-     for(int i=1; i<=n; i++){
-      fact = fact*i;
+    if(n<0){
+      System.out.println("Invalid number");
+      return;
     }
-    System.out.println("factorial = " + fact);
+    int factorial = 1;
+    for(int i=n; i>=1; i--){
+      factorial = factorial*i;
+    }
+    System.out.println(factorial);
+    return;
+
   }
+public static void main(String args[])
+
+{
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  factorial(n);
+}
 }
