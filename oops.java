efe1c56fd37ce4b31967e9308df30b3d60e5be65
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-13 21:38:04 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,33 @@
+public class oops{
+  public static void main(String args[]){
+    pen p1 = new pen();
+    p1.setColor("Pink");
+    System.out.println(p1.color);
+    p1.setTip(5);
+    System.out.println(p1.tip);
+    p1.color = "yellow";
+    System.out.println(p1.color);
+  }
+
+}
+
+class pen{
+  String color;
+  int tip;
+
+  void setColor(String newColor){
+    color = newColor;
+  }
+  void setTip(int newTip){
+    tip = newTip;
+  }
+}
+class Student {
+  String name;
+  int age;
+  float percentage;
+
+  void calcPercentage(int phy, int chem, int math){
+    percentage = (phy+chem+math)/3;
+  }
+}
