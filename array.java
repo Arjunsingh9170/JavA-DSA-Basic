9a92a17521624950117b5987d18e2778b2149eca
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-21 20:59:30 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array{
+  public static void main(String args[]){
+    int arr[] = {2, 4, 1, 8, 5, 9};
+    for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i] + " ");
+    }
+    
+
+  }
+}
---
+++ size.java
@@ -0,0 +1,7 @@
+public class size{
+  public static void main(String args[]){
+    int arr[] = {24, 12, 36, 5, 8, 9, 8,7, 10, 78, 45};
+    int size = arr.length;
+    System.out.print(size);
+  }
+}
