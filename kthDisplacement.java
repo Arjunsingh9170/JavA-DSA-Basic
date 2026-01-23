51d47de6afb846865ee36160f68b1c89d8d81ea7
Arjun singh <arjunasingharjunasingh192@gmail.com>
2026-01-23 19:00:08 +0530

    Add files via upload

---
+++ kthDisplacement.java
@@ -0,0 +1,9 @@
+public class kthDisplacement{
+  public static void main(String args[]){
+    int u = 10;
+    int a = 13; 
+    int k = 15; 
+    double displacement  = u + (a/2.0)*(2*k-1);
+    System.out.println(k + displacement); 
+  }
+}
